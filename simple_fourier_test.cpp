16c2aaf5e1dc4cd02c0b778191ff0ac3218dcefb
karan mittal <mittalkaran572@gmail.com>
2025-09-18 03:15:08 +0530

    simple_sine_oscillator

--- simple_fourier_test.cpp
+++ simple_fourier_test.cpp
@@ -3,7 +3,28 @@ Take a short audio clip (say 0.1–0.5 seconds), do a Fourier Transform, keep on
 */
 
 #include<iostream>
+#include<cmath>
+#include<fstream>
 using namespace std;
+
+const int sampleRate = 44100;
+
+class SineOscillator{
+    private:
+        int frequency;
+        int amplitude;
+        float angle = 0.0f;
+        float offset = 0.0f;
+    public:
+        SineOscillator(int freq, int amp) : frequency(freq), amplitude(amp) {
+            offset = 2 * M_PI * frequency / sampleRate;
+        };
+
+        float Process() {
+            auto smaple = amplitude * sin(angle);
+            angle += offset;
+        }
+};
 
 int main(){
     int x;
