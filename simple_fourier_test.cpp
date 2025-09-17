b4a9e6646e2e2b8ae7d7bc7ac57342c9e78a89ed
karan mittal <mittalkaran572@gmail.com>
2025-09-18 02:45:28 +0530

    setup

---
+++ simple_fourier_test.cpp
@@ -0,0 +1,10 @@
+/*
+Take a short audio clip (say 0.1–0.5 seconds), do a Fourier Transform, keep only the top terms, and paste into Desmos as a sum of sines.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+}
